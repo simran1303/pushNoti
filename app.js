c74c7aced0a4cf61e2e6ae196340fec47838be9f
simran1303 <simran19961303@gmail.com>
2022-09-04 14:50:48 +0530

    chng

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = require('./router/router');
-const sequelize = require('./util/database');
+//const sequelize = require('./util/database');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,16 +12,22 @@ app.use(express.json());
 
 app.use(router);
 
-sequelize
-.sync()
-.then(r =>{
-    console.log(r);
-    console.log('sync ');
-    app.listen(port, () => {
-        console.log(`Listening on port ${port}`);
-    })
+// sequelize
+// .sync()
+// .then(r =>{
+//     console.log(r);
+//     console.log('sync ');
+//     app.listen(port, () => {
+//         console.log(`Listening on port ${port}`);
+//     })
+// })
+// .catch(err => console.log(err));
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 })
-.catch(err => console.log(err));
 
 
    
